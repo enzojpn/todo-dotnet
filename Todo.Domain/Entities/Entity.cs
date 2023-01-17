757ca77f9db97af86fd38da3ca991b1d64918126
enzojpn <enzo_jpn@hotmail.com>
2023-01-17 20:25:17 -0300

    first commit

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,18 @@
+namespace Todo.Domain.Entity
+{
+
+    public abstract class Entity : IEquatable<Entity>
+    {
+
+        public Entity() 
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+
+        public bool Equals(Entity? other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
